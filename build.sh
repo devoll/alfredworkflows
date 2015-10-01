f21928641be9b5b21d741602cb940d709a8cc835
Pavel Nikitaev <p.nikitaev@mypay.co.th>
2015-10-01 13:06:52 +0700

    Added specified files

---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+cd parallels/src && zip -r ../parallels.alfredworkflow ./ -x "*.DS_Store"
---
+++ parallels/README.md
@@ -0,0 +1,5 @@
+# Workflow for manage Parallels VMs
+
+## How to use
+
+Type `prl` command
